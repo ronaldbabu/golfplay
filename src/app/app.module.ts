d8f19c398fd786e9c587b9fa5e4dd95fc291ce22
Ronald Babu <ronbabu007@gmail.com>
2019-02-15 13:59:05 +0530

    Push notification tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+import { InAppBrowser} from '@ionic-native/in-app-browser'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -22,7 +24,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
+    StatusBar,Push,InAppBrowser,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser'
+import { StatusBar } from '@ionic-native/status-bar';
+import { Platform } from 'ionic-angular';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,88 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  url: string;
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, private loadingCtrl: LoadingController, public platform: Platform, public statusBar: StatusBar, private push: Push, private alert: AlertController) {
+    platform.ready().then(() => {
+      if (platform.is('android')) {
+        statusBar.overlaysWebView(false);
+        statusBar.backgroundColorByHexString('#3e497f');
+      }
+    });
+
+    this.push.hasPermission()
+      .then((res: any) => {
+
+        if (res.isEnabled) {
+          console.log('We have permission to send push notifications');
+          this.initPush();
+        } else {
+          console.log('We do not have permission to send push notifications');
+        }
+
+      });
 
   }
+
+  //Push Notifications
+
+  initPush() {
+    const options: PushOptions = {
+      android: {},
+      ios: {
+        alert: 'true',
+        badge: true,
+        sound: 'false'
+      },
+      windows: {},
+      browser: {
+        pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+      }
+    };
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('notification').subscribe((notification: any) => 
+    {
+        const confirm = this.alert.create({
+          title: 'New Notification',
+          message: notification.message,
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              handler: () => {
+                console.log('Cancel clicked');
+              }
+            },
+            {
+              text: 'See',
+              handler: () => {
+                alert('See clicked');
+              }
+            }
+          ]
+        });
+        confirm.present();
+      
+    });
+
+    pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+
+    pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+  }
+
+  // Search Webpages
+
+  openWebpage(url) {
+
+    const options: InAppBrowserOptions = {
+      zoom: 'no'
+    }
+
+    const browser = this.iab.create(url, '_self', options);
+
+  }
+
+
 
 }
